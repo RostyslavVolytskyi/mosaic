af01b7ca1ff195b888001d8159b06e7540a30b33
rostyslav <rostyslav.volytskyi@symphony-solutions.eu>
2016-04-17 16:47:43 +0300

    forget ; in server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
   res.write('404 Not Found\n');
   res.end();
-  var port = Number(process.env.PORT || 8765)
+  var port = Number(process.env.PORT || 8765);
 }).listen(port, 'localhost');
 
 console.log('mosaic server running on port 8765');
